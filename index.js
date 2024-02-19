366ee5002828c4d9a04418520e9d22ffd8318365
etkosko <mrlahmacuns@gmail.com>
2024-02-19 03:45:09 +0300

    v1.01

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(cors());
 app.use(express.static('public'));
@@ -16,20 +16,12 @@ app.post('/get-website-screenshot', async (req, res) => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        // İlgili URL'yi açma ve sayfanın yüklenmesini beklemek
         await page.goto(url, { waitUntil: 'domcontentloaded' });
         await page.setViewport({ width: 1280, height: 800 });
-
-        // Sayfanın tamamen yüklenmesini beklemek
         await page.waitForNavigation({ waitUntil: 'load' });
-
-        // Ekran görüntüsü alma
         const screenshot = await page.screenshot();
-
-        // Tarayıcıyı kapatma
         await browser.close();
 
-        // Ekran görüntüsünü gönderme
         res.writeHead(200, {
             'Content-Type': 'image/png',
             'Content-Length': screenshot.length,
